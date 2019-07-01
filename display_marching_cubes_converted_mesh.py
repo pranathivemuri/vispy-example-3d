79be4eda1f8494e185a988ec546082213a45651f
pranathi.vemuri <pranathi93.vemuri@gmail.com>
2019-07-01 14:40:05 -0700

    fixed axes connection with the scene and location, appropriate scale factor so the turntable is displaying the mesh in the scale of vertices between the shape of the array not -1, 1

--- display_marching_cubes_converted_mesh.py
+++ display_marching_cubes_converted_mesh.py
@@ -2,10 +2,11 @@ import sys
 import numpy as np
 import skimage.measure
 from vispy import scene
-from vispy.color import Color
 from vispy.scene.cameras import TurntableCamera
 import vispy.io
 import vispy.geometry
+from vispy.visuals.transforms import STTransform
+
 
 ##
 # verts, faces, normals, nothin = vispy.io.read_mesh("mesh.obj")
@@ -23,19 +24,20 @@ canvas = scene.SceneCanvas(keys='interactive', size=(800, 600), show=True)
 # Set up a viewbox to display the cube with interactive arcball
 view = canvas.central_widget.add_view()
 view.bgcolor = '#efefef'
-view.camera = 'turntable'
-view.camera = TurntableCamera(fov=45)
-view.padding = 100
-view.camera.depth_value = 24
+# extent of the axis should fit twice as much as volume's x, y axis
+scale_factor = np.sqrt((surf.shape[0] ** 2 + surf.shape[1] ** 2)) * 2.0
+view.camera = TurntableCamera(fov=60, name='turntable', scale_factor=scale_factor)
 
-color = Color("#3f51b5")
-
-mesh = scene.visuals.Mesh(meshdata=meshdata, shading='smooth', color='w')
-mesh.set_gl_state('translucent', depth_test=True, cull_face=False)
+mesh = scene.visuals.Mesh(meshdata=meshdata, shading='smooth', color='w', parent=view.scene)
+mesh.set_gl_state('translucent', depth_test=True, cull_face=True)
 view.add(mesh)
 
 # Add a 3D axis to keep us oriented
 axis = scene.visuals.XYZAxis(parent=view.scene)
+s = STTransform(translate=(-100, -100), scale=(50, 50, 50, 1))
+affine = s.as_matrix()
+axis.transform = affine
+
 
 if __name__ == '__main__' and sys.flags.interactive == 0:
     canvas.app.run()
